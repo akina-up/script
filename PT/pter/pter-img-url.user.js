2b9bcff729b0d7df64eec2ed1782eb1aa6f09d42
akina-up <akina2up@proton.me>
2025-11-10 21:16:43 +0800

    feat pter-img-url

---
+++ PT/pter/pter-img-url.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         pter 修复旧域名图片
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  将旧域名的图片链接转为相对链接
+// @author       shadows,akina
+// @license      MIT License
+// @include      /^https?://pterclub\.net/.*$/
+// @icon         https://pterclub.net/favicon.ico
+// @downloadURL  https://cdn.jsdelivr.net/gh/akina-up/script@master/PT/pter/pter-img-url.user.js
+// @updateURL    https://cdn.jsdelivr.net/gh/akina-up/script@master/PT/pter/pter-img-url.user.js
+// @supportURL   https://github.com/akina-up/script/issues
+// ==/UserScript==
+'use strict';
+
+window.addEventListener('load', function () {
+  // 选中页面中所有图片
+  const imgs = document.querySelectorAll('img');
+
+  for (const img of imgs) {
+    if (img.src.includes('img.pterclub.com')) {
+      // 将旧域名替换成新域名
+      img.src = img.src.replace(/img\.pterclub\.com/gi, 'img.pterclub.net');
+    }
+  }
+});
