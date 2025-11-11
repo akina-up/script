8ea73e4174257c06a62964d5a094174a26ca93d3
akina-up <akina2up@proton.me>
2025-11-11 12:13:24 +0800

    Update pter-img-url to 0.3

--- PT/pter/pter-img-url.user.js
+++ PT/pter/pter-img-url.user.js
@@ -1,8 +1,7 @@
 // ==UserScript==
 // @name         pter 修复旧域名图片
 // @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  将旧域名的图片链接转为相对链接
+// @version      0.3
 // @author       shadows,akina
 // @license      MIT License
 // @include      /^https?://pterclub\.net/.*$/
@@ -13,19 +12,30 @@
 // ==/UserScript==
 
 /* 更新日志
+ * v0.3
+ * - [优化] 同时修复 data-src 与 data-orig 的旧域名
  * v0.2
  * - [优化] 扩大适配范围
  */
 'use strict';
 
-window.addEventListener('load', function () {
-  // 选中页面中所有图片
+window.addEventListener('load', () => {
   const imgs = document.querySelectorAll('img');
 
   for (const img of imgs) {
-    if (img.src.includes('pterclub.com')) {
-      // 将旧域名替换成新域名
+    // 修复 src
+    if (img.src && img.src.includes('pterclub.com')) {
       img.src = img.src.replace(/pterclub\.com/gi, 'pterclub.net');
+    }
+
+    // 修复 data-src
+    if (img.dataset.src && img.dataset.src.includes('pterclub.com')) {
+      img.dataset.src = img.dataset.src.replace(/pterclub\.com/gi, 'pterclub.net');
+    }
+
+    // 修复 data-orig
+    if (img.dataset.orig && img.dataset.orig.includes('pterclub.com')) {
+      img.dataset.orig = img.dataset.orig.replace(/pterclub\.com/gi, 'pterclub.net');
     }
   }
 });
